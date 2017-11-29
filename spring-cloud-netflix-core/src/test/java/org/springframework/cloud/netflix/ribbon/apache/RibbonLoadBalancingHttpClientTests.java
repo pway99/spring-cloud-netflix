6aec85630343235a1869aef3b015cbfa1c8cd2a2
pway99 <patway99@gmail.com>
2017-11-28 17:05:14 -0800

    Allowing RibbonLoadbalancedRetryPolicy to update ServerStats for circuit tripping exceptions

--- spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/ribbon/apache/RibbonLoadBalancingHttpClientTests.java
+++ spring-cloud-netflix-core/src/test/java/org/springframework/cloud/netflix/ribbon/apache/RibbonLoadBalancingHttpClientTests.java
@@ -256,7 +256,7 @@ public class RibbonLoadBalancingHttpClientTests {
 		doReturn(uriRequest).when(request).toRequest(any(RequestConfig.class));
 		RibbonApacheHttpResponse returnedResponse = client.execute(request, null);
 		verify(delegate, times(2)).execute(any(HttpUriRequest.class));
-		verify(lb, times(0)).chooseServer(eq(serviceName));
+		verify(lb, times(1)).chooseServer(eq(serviceName));
 	}
 
 	@Test
@@ -290,7 +290,7 @@ public class RibbonLoadBalancingHttpClientTests {
 		doReturn(uriRequest).when(request).toRequest(any(RequestConfig.class));
 		RibbonApacheHttpResponse returnedResponse = client.execute(request, null);
 		verify(delegate, times(3)).execute(any(HttpUriRequest.class));
-		verify(lb, times(1)).chooseServer(eq(serviceName));
+		verify(lb, times(2)).chooseServer(eq(serviceName));
 		assertEquals(2, myBackOffPolicyFactory.getCount());
 	}
 
@@ -325,7 +325,7 @@ public class RibbonLoadBalancingHttpClientTests {
 		RibbonApacheHttpResponse returnedResponse = client.execute(request, null);
 		verify(response, times(0)).close();
 		verify(delegate, times(3)).execute(any(HttpUriRequest.class));
-		verify(lb, times(1)).chooseServer(eq(serviceName));
+		verify(lb, times(2)).chooseServer(eq(serviceName));
 		assertEquals(2, myBackOffPolicyFactory.getCount());
 	}
 
@@ -360,7 +360,7 @@ public class RibbonLoadBalancingHttpClientTests {
 		} catch(IOException e) {} finally {
 			verify(response, times(0)).close();
 			verify(delegate, times(1)).execute(any(HttpUriRequest.class));
-			verify(lb, times(0)).chooseServer(eq(serviceName));
+			verify(lb, times(1)).chooseServer(eq(serviceName));
 		}
 	}
 
@@ -399,7 +399,7 @@ public class RibbonLoadBalancingHttpClientTests {
 		RibbonApacheHttpResponse returnedResponse = client.execute(request, null);
 		verify(fourOFourResponse, times(1)).close();
 		verify(delegate, times(2)).execute(any(HttpUriRequest.class));
-		verify(lb, times(0)).chooseServer(eq(serviceName));
+		verify(lb, times(1)).chooseServer(eq(serviceName));
 		assertEquals(1, myBackOffPolicyFactory.getCount());
 	}
 
